d85ceff192c31d1ecf74d89255d7a23575dcac30
Mike Mackintosh <mmackintosh@shutterstock.com>
2014-03-10 16:04:59 -0400

    adding dir stuff

--- profile.d/rover.sh
+++ profile.d/rover.sh
@@ -1,2 +1,19 @@
+#
+## Managed by Rocketho.me
+## Revision 10 Mar 2014
+
+#
+#-- Set IP Env stuff
+
+# Extract the local IP
 export IP=$(/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
-export EIP=$(curl icanhazip.com -s)
+
+# Extract the External IP
+export EIP=$(curl -s icanhazip.com)
+
+#
+#-- Set Directory Stuff
+
+# Set WWW dir
+export WWWDIR='/var/www/'
+export GITDIR='/var/git/'
